66706a0346092d732de6439e522a166676083eea
Iacopo Spalletti <github@spalletti.it>
2019-10-24 13:31:14 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -106,11 +106,6 @@ pygments_style = 'sphinx'
 
 # -- AutoStructify --------------------------------------------------------
 def setup(app):
-    app.add_config_value('recommonmark_config', {
-        'auto_toc_tree_section': 'Contents',
-        'enable_eval_rst': True,
-        'enable_auto_doc_ref': True
-    }, True)
     app.add_transform(AutoStructify)
 
 
